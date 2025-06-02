fea84cb5c5642ffb29aae4799c60279e1c9d3d55
Jeffrey Zhai <1937325843@qq.com>
2025-06-01 20:54:47 -0700

    renamed url

---
+++ StudyB/config.js
@@ -0,0 +1,5 @@
+window.config = {
+    userID: 'ovfFI5vgYgZElcRGw',
+    serviceID: 'service_svpfi0i',
+    templateID: 'template_s213h8p'
+  };
--- StudyB_FN/config.js
+++
@@ -1,5 +0,0 @@
-window.config = {
-    userID: 'ovfFI5vgYgZElcRGw',
-    serviceID: 'service_svpfi0i',
-    templateID: 'template_s213h8p'
-  };
